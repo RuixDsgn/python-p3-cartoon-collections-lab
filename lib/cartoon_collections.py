7d4d2e3a1ce1cdb06fa6f1f776a22a259a00d183
Carla <113267384+RuixDsgn@users.noreply.github.com>
2023-06-18 12:57:23 -0700

    complete lab with all tests passing

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,26 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+    for i, name in enumerate(names, start=1):
+        print(f"{i}. {name}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(lists):
+    planateer_calls = [f"{list.title()}!" for list in lists]
+    
+    return planateer_calls
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(string_list):
+    for list in string_list:
+        if len(list) > 4:
+            return True
+    else:
+        return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(foods):
+    for food in foods:
+        if "cheddar" in foods:
+            return "cheddar"
+        elif "gouda" in foods:
+            return "gouda"
+        elif "camembert" in foods:
+            return "camembert"
+        else:
+            return None
